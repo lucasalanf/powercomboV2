153b24b3b199fd6d9ef4eafdb3145ce6b829f818
lucasalanf <lucas.a.freitas@outlook.com>
2017-03-23 20:11:58 -0300

    inserção pagina combate e vivo-internet

---
+++ paginasStaticas/combate.php
@@ -0,0 +1,22 @@
+<?php include '../header.php' ?>
+<div class="conteudoPrincipal row">
+<div class="container">
+<blockquote>
+<p><span style="font-size: 12.0pt; line-height: 115%;">Os canais <strong>A La Carte são</strong> “produtos” contratados e pagos adicionalmente à programação principal, ou seja, você possui um pacote e se quiser o canal Combate, em especial, para variar sua atual programação e conseguir curtir programas em HD, você contrata somente esse canal, de modo avulso, sem precisar aumentar ou mudar de pacote principal.</span></p>
+</blockquote>
+
+<p><img class="alignnone wp-image-1125" src="http://powercombo.com.br/wp-content/uploads/2016/08/combate-300x188.jpg" alt="combate" width="586" height="367" /></p>
+<p><strong>Isso não é incrível?</strong></p>
+
+<p>Sabia que contratando o canal <strong>Combate</strong> você tem a maior cobertura dos melhores e maiores eventos de lutas nacionais e internacionais?</p>
+
+<p>Você conseguirá acompanhar de perto eventos, como: <strong>UFC</strong>, <strong>K-1</strong>, além de conferir e acompanhar modalidades, como <strong>Boxe</strong>, <strong>Jiu jtsu</strong>, <strong>Judô</strong>, <strong>Muay thai</strong>, <strong>Submission</strong>, <strong>Capoeira</strong>,<strong>Wrestling</strong>, <strong>Caratê</strong>, <strong>Taekwondo</strong> e muitas das várias que o canal disponibiliza.</p>
+
+<p>A programação do canal Combate e imbatível e gosta de dar destaque à adrenalina que envolve os eventos de lutas, então você poderá sentir a emoção como se estivesse lutando.</p>
+
+<p>Programação diversificada com faixas especiais, equipe especializada de comentaristas e narradores que garantirá que você sinta a emoção da luta de onde estiver assistindo, além de várias informações atuais sobre o mundo da luta.</p>
+
+<p>Como é simples de ver, o canal reúne o melhor das lutas em um único lugar, a qualidade é garantida e você pode ver toda a <strong>programação em HD</strong>.</p>
+</div>
+</div>
+ <?php include '../footer.php' ?>
---
+++ paginasStaticas/vivo-internet.php
@@ -0,0 +1,71 @@
+<?php include '../header.php' ?>
+ <?php include '../planosHome.php' ?>
+
+<div class="conteudoPrincipal row">
+<div class="container">
+	<p>A <strong>Banda Larga Vivo</strong> é considerada a melhor banda larga do Brasil por seis anos consecutivos de acordo com a pesquisa aberta realizada com leitores da <strong>revista INFO</strong>. <strong>A Vivo</strong> opera com velocidades de<strong> banda larga real</strong>, não nominal. Ou seja, a velocidade entregue de download é a velocidade contratada.</p>
+<p>É também, segundo o ranking de 2013 da revista <strong>IsoÉ Dinheiro</strong>, a melhor operadora do setor de telecomunicações</p>
+<p>Além de receber o prêmio <b>as empresas que mais respeitam o consumidor </b>nos anos de 2012 e 2013.</p>
+
+<p>A Banda Larga Vivo <b>não tem limite de transmissão de dados </b>e entrega a velocidade contratada.</p>
+
+
+
+<strong>Possuímos pacotes com velocidades de até:</strong>
+<div class="boxCanais boxInternet">
+<div class="leftCanais">
+<div class="alinhaCentro">50Mega</div>
+</div>
+<div class="rightCanais">
+
+- Instalação e configuração do Modem<br>
+- Download: 50 Mbps<br>
+- Upload: Até 25 Mbps<br>
+- Consulte a velocidade na sua cidade
+
+</div>
+</div>
+<div class="boxCanais boxInternet">
+<div class="leftCanais">
+<div class="alinhaCentro">100Mega</div>
+</div>
+<div class="rightCanais">
+
+- Instalação e configuração do Modem<br>
+- Download: 100 Mbps<br>
+- Upload: Até 50 Mbps<br>
+- Consulte a velocidade na sua cidade
+
+</div>
+</div>
+<div class="boxCanais boxInternet">
+<div class="leftCanais">
+<div class="alinhaCentro">200Mega</div>
+</div>
+<div class="rightCanais">
+
+- Instalação e configuração do Modem<br>
+- Download: 200 Mbps<br>
+- Upload: Até 100 Mbps<br>
+- Consulte a velocidade na sua cidade
+
+</div>
+</div>
+<div class="boxCanais boxInternet">
+<div class="leftCanais">
+<div class="alinhaCentro">300Mega</div>
+</div>
+<div class="rightCanais">
+
+- Instalação e configuração do Modem<br>
+- Download: 300 Mbps<br>
+- Upload: Até 150 Mbps<br>
+- Consulte a velocidade na sua cidade
+
+</div>
+</div>
+
+
+</div>
+</div>
+ <?php include '../footer.php' ?>
